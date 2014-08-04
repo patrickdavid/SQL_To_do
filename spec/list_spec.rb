4efc2c3352f3614b2f4f06d4a58505ae130cd6a0
Peggy Condon & patrick david <student@epicodus.com>
2014-08-04 11:32:59 -0700

    Adds list class which initializes its instances

---
+++ spec/list_spec.rb
@@ -0,0 +1,19 @@
+
+require 'list'
+require 'rspec'
+require 'pg'
+
+DB = PG.connect(:dbname => 'to_do_test')
+
+Rspec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM lists *;")
+  end
+end
+
+describe List do
+  it'is initialized with a name' do
+    list = List.new('Epicodus Stuff')
+    expect(list).to be_an_instance_of List
+  end
+end
